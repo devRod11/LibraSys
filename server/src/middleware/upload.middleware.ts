30739fc71ad769801498db9463dc40e3469343b4
devRod11 <bigtas.rodic@gmail.com>
2026-05-21 10:04:07 +0800

    Update upload.middleware.ts

--- server/src/middleware/upload.middleware.ts
+++ server/src/middleware/upload.middleware.ts
@@ -1,31 +1,21 @@
 import multer from "multer";
+import { Request } from "express";
 
 const storage = multer.memoryStorage();
 
 export const upload = multer({
-
   storage,
-
   limits: {
-    fileSize: 2 * 1024 * 1024,
+    fileSize: 2 * 1024 * 1024, // 2MB
   },
 
   fileFilter: (
-    req,
-    file,
-    cb
+    req: Request,
+    file: Express.Multer.File,
+    cb: multer.FileFilterCallback
   ) => {
-
-    if (
-      file.mimetype !==
-      "text/csv"
-    ) {
-
-      return cb(
-        new Error(
-          "Only CSV files allowed"
-        )
-      );
+    if (file.mimetype !== "text/csv") {
+      return cb(new Error("Only CSV files allowed"));
     }
 
     cb(null, true);
