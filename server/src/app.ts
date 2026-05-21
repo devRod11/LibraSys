59a9e29540a3ce446f962213c61248c58fcec5d0
devRod11 <bigtas.rodic@gmail.com>
2026-05-21 09:02:36 +0800

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://librasys-10.onrender.com",
   credentials: true
 }));
 
