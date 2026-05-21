49b99ff681ba77a7b29951e8aea0e0e074a6ccfb
devRod11 <bigtas.rodic@gmail.com>
2026-05-21 09:10:35 +0800

    Update index.ts

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
