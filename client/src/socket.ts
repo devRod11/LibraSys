7c72504f7f3070a54fb5ebbca24d74d4b435c4c3
devRod11 <bigtas.rodic@gmail.com>
2026-05-21 09:18:36 +0800

    Update socket.ts

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,8 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io(
-  "http://localhost:3001",
-  {
-    withCredentials: true,
-  }
-);
+export const socket = io(import.meta.env.VITE_API_URL, {
+  transports: ["websocket", "polling"],
+});
