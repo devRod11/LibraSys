79c906c6ab08b5a6648d6b7e971c60b7a54f1fde
devRod11 <bigtas.rodic@gmail.com>
2026-05-23 01:20:34 +0800

    Update socket.ts

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://librasys-v9.onrender.com", {
+export const socket = io("https://librasys-backend.onrender.com", {
   transports: ["websocket", "polling"],
   withCredentials: true,
 });
