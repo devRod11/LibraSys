3e53fd76d652d8dc68102e3b2933920394b1ec9f
devRod11 <bigtas.rodic@gmail.com>
2026-05-23 01:25:08 +0800

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ["https://librasys-10.onrender.com"],
+  origin: ["https://librasys-j7xy.onrender.com"],
   credentials: true
 }));
 
