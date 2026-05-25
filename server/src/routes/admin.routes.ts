41f606ba334f24eaef5343b6ac5ed6d981e0a910
devRod11 <bigtas.rodic@gmail.com>
2026-05-25 09:27:50 +0800

    Update admin.routes.ts

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -1,17 +1,13 @@
 import express from "express";
-import { bulkUploadStudents, } from "../controllers/admin.controller";
+import { bulkUploadStudents,getStudents,deleteStudent, } from "../controllers/admin.controller";
 import { verifyToken, } from "../middleware/auth.middleware";
 import { upload, } from "../middleware/upload.middleware";
 import { bulkUploadLimiter, } from "../middleware/rateLimit.middleware";
 
 const router = express.Router();
 
-router.post(
-  "/students/bulk-upload",
-  verifyToken,
-  bulkUploadLimiter,
-  upload.single("file"),
-  bulkUploadStudents
-);
+router.post("/students/bulk-upload", verifyToken, bulkUploadLimiter, upload.single("file"), bulkUploadStudents);
+router.get("/students", verifyToken, getStudents);
+router.delete("/students/:id", verifyToken, deleteStudent);
 
 export default router;
