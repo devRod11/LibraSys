b67a3117d9f7b7400fc14266d3885e34fcff7c8e
devRod11 <bigtas.rodic@gmail.com>
2026-05-25 09:30:29 +0800

    Update user.routes.ts

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,14 +1,6 @@
 import express from "express";
-
-import {
-  getMe,
-  updateMe,
-  changePassword,
-} from "../controllers/user.controller";
-
-import {
-  verifyToken,
-} from "../middleware/auth.middleware";
+import {getMe, updateMe, changePassword,} from "../controllers/user.controller";
+import  verifyToken,} from "../middleware/auth.middleware";
 
 const router = express.Router();
 
